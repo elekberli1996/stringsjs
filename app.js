567c9efc3f30d207ba15739ae8988594303d7f14
emin elekberli <elekberli1996@gmail.com>
2024-03-25 20:37:42 +0400

    commit

---
+++ app.js
@@ -0,0 +1,39 @@
+ 
+//  //task1 adiniz nece sinvoldur
+//  let name = prompt("adinizi daxil edin");
+//  console.log(name.length);
+
+// //task2 girilen simvol girilen sozde varmi
+// let soz=prompt("bir sozu daxil edin");
+// let simvol= prompt("bir simvol girin ")
+// console.log(soz.includes(simvol));
+
+
+// //task3 girlilen simvol girilien sozun 1ci herfidirmi
+// let soz=prompt("bir sozu daxil edin");
+// let simvol= prompt("bir simvol girin ")
+// console.log(soz.startsWith(simvol));
+
+
+// //task4 girilen simvolun girilien sozun sonuncu herfidirmi
+// let soz=prompt("bir sozu daxil edin");
+// let simvol= prompt("bir simvol girin ")
+// console.log(soz.endsWith(simvol));
+
+// //task5 girilen sinvolun girilen sozdeki indeksini tap
+// let soz=prompt("bir sozu daxil edin");
+// let simvol= prompt("bir simvol girin ");
+//  console.log(soz.indexOf(simvol));
+
+// //task6  Sozun boyuk herlfli olub olmadiqini teyin et
+// const isUpperCase = str => str === str.toUpperCase();
+// let soz=prompt("bir sozu daxil edin");
+// console.log(isUpperCase(soz));
+
+
+
+
+
+
+
+
